0e25d951c944cbf8a1a4fbc7240bc477e291f27e
faustogengaalfaro@gmail.com <faustogengaalfaro@gmail.com>
2024-03-06 12:34:15 -0600

    update vite

---
+++ src/home.jsx
@@ -0,0 +1,3 @@
+export const home = () => {
+    <h1>FG Portfolio</h1>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import home from './home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <home/>
   </React.StrictMode>,
 )
