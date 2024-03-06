ea0afe301a9a26dbb0e6cef344e777dd820c1a02
faustogengaalfaro@gmail.com <faustogengaalfaro@gmail.com>
2024-03-06 12:37:38 -0600

    fixed and running

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import home from './home'
+import ReactDOM from 'react-dom'
+import { Home } from './Home'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <home/>
+    <Home/>
   </React.StrictMode>,
 )
